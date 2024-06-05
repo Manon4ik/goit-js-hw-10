cd02e583da82e83784e252b64308f9ca0285eec4
Misha <misha.n@thewhitelabelagency.com>
2024-06-05 18:22:20 +0300

    start hw-10

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import './01-color-switcher'
+import './02-timer'
+import './03-promises'
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    entry: './js/main.js',
+    mode: 'production',
+    module: {
+        rules: [
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.(js)$/, use: 'babel-loader' }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        clean: true
+    },
+}
